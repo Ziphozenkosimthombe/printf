92ce05d4209b8ef10ed3210e3bfee40e5e06b75f
Uzor Godstime <122891415+NecheU@users.noreply.github.com>
2023-03-27 22:19:57 +0100

    Update main.h

--- main.h
+++ main.h
@@ -8,12 +8,14 @@
 #define BUFF_SIZE 1024
 
 
+/* FLEGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
+/* SIZES */
 
 #define S_LONG 2
 #define S_SHORT 1
