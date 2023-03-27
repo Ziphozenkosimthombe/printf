7233b2c022fe01c74187012c1dc4a3ff64188c19
Uzor Godstime <122891415+NecheU@users.noreply.github.com>
2023-03-27 21:25:22 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -26,6 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+                        /* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
