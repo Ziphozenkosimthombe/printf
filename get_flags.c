7e7d2666dc0ecb8e5c0386b5a451ec047918ec19
Uzor Godstime <122891415+NecheU@users.noreply.github.com>
2023-03-27 22:04:51 +0100

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -9,6 +9,7 @@
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
+        /* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
