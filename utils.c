04d2e6d5425ba9601c7604ab256abf71d6e01afd
ziphozenkos <ziphomthombe478@gmail.com>
2023-03-28 07:45:07 -0700

    update

--- utils.c
+++ utils.c
@@ -24,7 +24,7 @@ int is_printable(char c)
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-        /* The hexa format code is always 2 digits long */
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
